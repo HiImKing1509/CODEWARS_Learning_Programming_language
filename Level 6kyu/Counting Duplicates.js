3011debc443cbaaebdfa2c957cd183c2b9ca45e0
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-06-11 23:47:42 +0700

    Practice JavaScript
    https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1

--- Level 6kyu/Counting Duplicates.js
+++ Level 6kyu/Counting Duplicates.js
@@ -1,1 +1,14 @@
-
+function duplicateCount(text) {
+    var obj = {};
+    var res = 0;
+    text.toLowerCase().split("").forEach(element => {
+        if (element in obj)
+            obj[element]++;
+        else
+            obj[element] = 1;
+    });
+    for (const key in obj)
+        if (obj[key] > 1)
+            res++;
+    return res;
+}
