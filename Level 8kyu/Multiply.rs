a50bb2ee1ab1e2c405dcd4d88186bece93dad07f
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-10-19 10:44:59 +0700

    Learning Rust language

---
+++ Level 8kyu/Multiply.rs
@@ -0,0 +1,12 @@
+// Submitted
+fn multiply(a:i32, b:i32) -> i32 {
+    return a * b;
+}
+
+// Run to test
+fn main() {
+    let mut a = 2;
+    let mut b = 3;
+    let res = multiply(a, b);
+    println!("{}", res);
+}
